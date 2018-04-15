5e926893d21b7bd8508f9b5645be2e5b8c5db3bb
Alex Storm <alex.ellegaard@yahoo.com>
2018-04-15 21:01:04 +0200

    1.0.1 finished

---
+++ app/src/main/java/stormeal/birdwatchingupdated/ObservationsAdd.java
@@ -0,0 +1,13 @@
+package stormeal.birdwatchingupdated;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ObservationsAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observations_add);
+    }
+}
---
+++ app/src/main/java/stormeal/birdwatchingupdated/ReadHttpTask.java
@@ -0,0 +1,27 @@
+package stormeal.birdwatchingupdated;
+
+/**
+ * Created by Alex on 15-04-2018.
+ */
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+
+
+public class ReadHttpTask extends AsyncTask<String, Void, CharSequence> {
+    @Override
+    protected CharSequence doInBackground(String... urls) {
+        String urlString = urls[0];
+        try {
+            CharSequence result = HttpHelper.GetHttpResponse(urlString);
+            return result;
+        } catch (IOException ex) {
+            cancel(true);
+            String errorMessage = ex.getMessage() + "\n" + urlString;
+            Log.e("BOOK", errorMessage);
+            return errorMessage;
+        }
+    }
+}
